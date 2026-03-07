6fde837624f3b5d5b2c3855fd06ad35051d3d76d
zerlok <danil.troshnev@gmail.com>
2026-03-08 00:00:36 +0100

    fix get_metavar for old python version in PythonVersionParamType

--- src/astlab/click.py
+++ src/astlab/click.py
@@ -1,3 +1,4 @@
+import sys
 import typing as t
 
 import click
@@ -27,9 +28,17 @@ class PythonVersionParamType(click.ParamType):
         choices = ", ".join(str(v) for v in self.__iter_versions())
         self.fail(f"invalid python version {value!r}, expected one of: {choices}", param, ctx)
 
-    @override
-    def get_metavar(self, param: click.Parameter, ctx: click.Context) -> str:  # noqa: ARG002
-        return "|".join(str(v) for v in self.__iter_versions())
+    if sys.version_info >= (3, 10):
+
+        @override
+        def get_metavar(self, param: click.Parameter, ctx: click.Context) -> str:  # noqa: ARG002
+            return "|".join(str(v) for v in self.__iter_versions())
+
+    else:
+
+        @override
+        def get_metavar(self, param: click.Parameter) -> str:  # noqa: ARG002
+            return "|".join(str(v) for v in self.__iter_versions())
 
     def __iter_versions(self) -> t.Iterable[PythonVersion]:
         return PythonVersion.get_all_supported(ignore_outdated_runtime=self.__ignore_outdated_runtime)
